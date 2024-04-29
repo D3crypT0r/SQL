a733054d269717dc1d91a8e3e530ab57f4085303
D3crypT0r (Syed Rizwan Hilal Shah) <66831571+D3crypT0r@users.noreply.github.com>
2024-04-29 11:06:54 +0530

    Update example.sql

--- example.sql
+++ example.sql
@@ -6,6 +6,22 @@ CREATE TABLE BCA_Students (
     PhoneNumber VARCHAR(15)
 );
 
+INSERT INTO BCA_Students (Name, parentage, address, age, Phone Number)
+
+VALUES ('Rizwan', 'Hilal', 'Kupwara', 25, 7051664737');
+
+
+
+INSERT INTO BCA_Students (Name, parentage, address, age, Phone Number)
+
+VALUES ('Shahid', 'Showkat', 'GBL', 21, '7051661234');
+
+
+
+INSERT INTO BCA Students (Name, parentage, address, age, PhoneNumber)
+
+VALUES ('Aadil', 'Iqbal', 'Ganderbal', 21, '7051665678');
+
 
 
 
